397a115fc6482aa96480fa47fd041007e38507ba
Olidev <jean.yantas@gmail.com>
2023-11-02 10:42:19 -0500

    Add new file

---
+++ src/main/java/com/olidev/pe/cosmosapirest/CosmosApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.olidev.pe.cosmosapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmosApiRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CosmosApiRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/olidev/pe/cosmosapirest/CosmosApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olidev.pe.cosmosapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmosApiRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
